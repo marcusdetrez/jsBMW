54f6f0d028ed6f29b873f3901589d3526fcdea66
marcusdetrez <marcus.detrez@gmail.com>
2018-11-12 15:40:04 +0100

    update de tout les fichiers

---
+++ codejs.js
@@ -0,0 +1,9 @@
+var popup;
+
+function opnapopup() {
+	popup = setTimeout(alertfunction() , 1000);
+}
+
+function alertfunction() {
+	alert("Hey le site xxxvidsxxx est trop bien. Viens dessus stp please");
+}
